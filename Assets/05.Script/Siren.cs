761d43a10c182c30af1f0efbe2500d7e97bab42d
양수민 <oper1782@naver.com>
2017-03-17 18:09:50 +0900

    사이렌

---
+++ Assets/05.Script/Siren.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Siren : MonoBehaviour {
+    Behaviour halo;
+    public Material col;
+    // Use this for initialization
+    void Start () {
+        halo = (Behaviour)GetComponent("Halo");
+        StartCoroutine(light());
+        col.color = Color.white;
+        halo.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        
+	}
+    IEnumerator light()
+    {
+        while (true) { 
+        yield return new WaitForSeconds(0.3f);
+
+        if (halo.enabled == true)
+        {
+            col.color = Color.white;
+            halo.enabled = false;
+        }
+        else
+        {
+            col.color = Color.red;
+            halo.enabled = true;
+        }
+        }
+    }
+}
